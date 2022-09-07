d2c1583af891919027b0ed9df9b69b78d759db4b
William Fernandes Dias <william.winchester1967@gmail.com>
2022-09-07 15:41:46 -0300

    feat: adicionar página para login

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page padding>
+    <LoginButton />
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import LoginButton from 'components/LoginButton.vue';
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,12 +3,21 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    name: 'index',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
   {
+    path: '/login',
+    name: 'login',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/AuthPage.vue') }],
+  },
+
+  {
     path: '/robot/parameters',
+    name: 'parameters',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
@@ -20,6 +29,7 @@ const routes: RouteRecordRaw[] = [
 
   {
     path: '/robot/mapping',
+    name: 'mapping',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {

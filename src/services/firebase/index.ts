6628a1d885f147f7acaff9af395d866ebd584697
William Fernandes Dias <william.winchester1967@gmail.com>
2022-09-07 15:45:00 -0300

    feat: adicionar firebase à dashboard

---
+++ src/services/firebase/index.ts
@@ -0,0 +1,22 @@
+import { inject } from 'vue';
+import type { App } from 'vue';
+import initFirebaseApp from './app';
+import getAuthService from './auth';
+
+export const plugin = {
+  install(app: App) {
+    const firebaseApp = initFirebaseApp();
+    const firebase = {
+      app: firebaseApp,
+      auth: getAuthService(firebaseApp),
+    };
+
+    // Adiciona aplicação do firebase no escopo global da dashboard
+    app.config.globalProperties.$firebase = firebase;
+    app.provide(key, firebase);
+  },
+};
+
+// Definir hook para injetar aplicação do firebase em qualquer componente
+const key = Symbol('firebase');
+export default () => inject<LFCommandCenter.FirebaseBackend>(key);

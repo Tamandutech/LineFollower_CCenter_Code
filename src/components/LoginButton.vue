2c205f7d8e5e6c04449bbdd6d1803d554395981e
William Fernandes Dias <william.winchester1967@gmail.com>
2022-09-07 15:37:03 -0300

    feat: implementar botão para login via Github

---
+++ src/components/LoginButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-btn color="secondary" @click="login">
+    <slot>Login</slot>
+  </q-btn>
+</template>
+
+<script lang="ts" setup>
+import { useAuth } from 'stores/auth';
+import useFirebase from 'src/services/firebase';
+import { useDebounceFn } from '@vueuse/core';
+
+const auth = useAuth();
+const {
+  auth: { service, provider },
+} = useFirebase();
+
+const login = useDebounceFn(() => auth.loginUser({ service, provider }), 5000);
+</script>

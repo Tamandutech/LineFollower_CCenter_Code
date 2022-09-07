8a359371657e153f05811e76c9bb0f19b404d691
William Fernandes Dias <william.winchester1967@gmail.com>
2022-09-07 15:47:34 -0300

    implementar store para usuários

---
+++ src/stores/auth.ts
@@ -0,0 +1,45 @@
+import { defineStore } from 'pinia';
+import { signInWithPopup, signOut, GithubAuthProvider } from 'firebase/auth';
+import type { User, Auth } from 'firebase/auth';
+
+export const useAuth = defineStore('auth', {
+  state: () => ({
+    user: null,
+  }),
+  getters: {
+    getCurrentUser(state) {
+      return state.user;
+    },
+  },
+  actions: {
+    async loginUser(auth: LFCommandCenter.AuthService): Promise<User | Error> {
+      try {
+        const result = await signInWithPopup(auth.service, auth.provider);
+        const token = GithubAuthProvider.credentialFromResult(result);
+
+        const response = await fetch('https://api.github.com/user/memberships/orgs/Tamandutech', {
+          method: 'GET',
+          headers: { Authorization: `Bearer ${token}`, Accept: 'application/vnd.github+json' },
+        });
+        const membership = (await response.json()) as { state: string };
+        if (membership.state !== 'active') {
+          this.logoutUser(auth.service);
+          throw new Error('Acesso negado. Permitido apenas para membros da Tamandutech.');
+        }
+
+        this.user = result.user;
+        return Promise.resolve<User>(this.user);
+      } catch (error) {
+        return Promise.reject<Error>(error);
+      }
+    },
+    async logoutUser(service: Auth): Promise<void> {
+      return signOut(service);
+    },
+    async handleOnAuthStateChanged(currentUser: User | null) {
+      this.$patch((state) => {
+        state.user = currentUser;
+      });
+    },
+  },
+});

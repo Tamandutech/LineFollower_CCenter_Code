24651a442ae1ffa0574b8094415ba2cbeaad030c
William Fernandes Dias <william.winchester1967@gmail.com>
2022-09-07 15:34:54 -0300

    feat: implementar script para instalar serviços do firebase na dashboard

---
+++ src/boot/main.ts
@@ -0,0 +1,23 @@
+import { plugin } from 'src/services/firebase';
+import type { User } from 'firebase/auth';
+import { useAuth } from 'stores/auth';
+import { boot } from 'quasar/wrappers';
+
+export default boot(async ({ app, router }) => {
+  app.use(plugin);
+
+  const {
+    auth: { service },
+  } = app.config.globalProperties.$firebase;
+  const auth = useAuth();
+  auth.user = service.currentUser;
+  service.onAuthStateChanged(async (user: User | null) => {
+    await auth.handleOnAuthStateChanged(user);
+  });
+
+  router.beforeEach((to) => {
+    if (!auth.user && to.path !== '/login') {
+      return { path: '/login' };
+    }
+  });
+});

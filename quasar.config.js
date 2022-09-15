b115c2f4954f1c9e06aba65d81cf74878d7a0a4e
Raphael Freitas <50784405+raphera@users.noreply.github.com>
2022-09-15 17:30:33 -0300

    change: wait reload to update.

--- quasar.config.js
+++ quasar.config.js
@@ -175,7 +175,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
     pwa: {
       workboxMode: 'generateSW', // or 'injectManifest'
-      workboxOptions: { skipWaiting: true },
+      // workboxOptions: { skipWaiting: true },
       injectPwaMetaTags: true,
       swFilename: 'sw.js',
       manifestFilename: 'manifest.json',

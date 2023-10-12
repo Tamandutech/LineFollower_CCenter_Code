99bc4c17db9b7f2abe13f57219d2f8ea3ff4cfbd
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-12 12:23:11 -0300

    STYLE: Formatando o MappingPage no padrão do Prettier e ESLint

--- src/pages/MappingPage.vue
+++ src/pages/MappingPage.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="q-pa-md">
-    <q-table title="Mapeamento" :rows="mappingRecords" :columns="columns" row-key="id" binary-state-sort>
+    <q-table
+      title="Mapeamento"
+      :rows="mappingRecords"
+      :columns="columns"
+      row-key="id"
+      binary-state-sort
+    >
       <template v-slot:top-left>
-        <q-btn :icon="mdiSourceBranch" flat @click="toogleVersionsDialog(true)" :disable="!ble.connected" />
+        <q-btn
+          :icon="mdiSourceBranch"
+          flat
+          @click="toogleVersionsDialog(true)"
+          :disable="!ble.connected"
+        />
       </template>
       <template v-slot:body="props">
         <q-tr :props="props">
@@ -11,66 +22,123 @@
           </q-td>
           <q-td key="EncMedia" :props="props">
             {{ props.row.encMedia }}
-            <q-popup-edit v-model="props.row.encMedia" title="Atualizar Média dos encoders" buttons v-slot="scope">
+            <q-popup-edit
+              v-model="props.row.encMedia"
+              title="Atualizar Média dos encoders"
+              buttons
+              v-slot="scope"
+            >
               <q-input type="number" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
           <q-td key="Time" :props="props">
             {{ props.row.time }}
-            <q-popup-edit v-model="props.row.time" title="Atualizar o tempo" buttons v-slot="scope">
+            <q-popup-edit
+              v-model="props.row.time"
+              title="Atualizar o tempo"
+              buttons
+              v-slot="scope"
+            >
               <q-input type="number" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
 
           <q-td key="Offset" :props="props">
             {{ props.row.offset }}
-            <q-popup-edit v-model="props.row.offset" title="Atualizar Offset" buttons v-slot="scope">
+            <q-popup-edit
+              v-model="props.row.offset"
+              title="Atualizar Offset"
+              buttons
+              v-slot="scope"
+            >
               <q-input type="number" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
 
           <q-td key="TrackStatus" :props="props">
             {{ sectionOftheTrackToText(props.row.trackStatus) }}
-            <q-popup-edit v-model="props.row.trackStatus" title="Atualizar o Trackstatus" buttons v-slot="scope">
-              <q-select filled v-model="scope.value" :options="trackStatusOptions" emit-value map-options />
+            <q-popup-edit
+              v-model="props.row.trackStatus"
+              title="Atualizar o Trackstatus"
+              buttons
+              v-slot="scope"
+            >
+              <q-select
+                filled
+                v-model="scope.value"
+                :options="trackStatusOptions"
+                emit-value
+                map-options
+              />
             </q-popup-edit>
           </q-td>
         </q-tr>
       </template>
     </q-table>
     <div class="q-pa-md q-gutter-sm">
-      <q-btn @click="
-        withSuccessFeedback(sendMapping, {
-          summary: 'Mapeamento Enviado',
-          message: 'O mapeamento foi enviado para o robô com sucesso!',
-        })()
-        " color="primary" label="Enviar mapeamento" :disable="loading !== null || mappingRecords.length === 0" />
-      <q-btn @click="fetchMapping(false)" color="primary" label="Ler mapeamento" />
-      <q-btn @click="fetchMapping(true)" color="primary" label="Ler mapeamento na Ram" />
-      <q-btn @click="
-        performAction(
-          withSuccessFeedback(saveMapping, {
-            summary: 'Mapeamento Salvo',
-            message: 'O mapeamento enviado foi registrado com sucesso!',
-          }),
-          [],
-          {
-            title: 'Salvar Mapeamento',
-            question: 'Tem certeza que deseja salvar o mapeamento atual?',
-          }
-        )
-        " color="primary" label="Salvar mapeamento" :disable="loading !== null" />
+      <q-btn
+        @click="
+          withSuccessFeedback(sendMapping, {
+            summary: 'Mapeamento Enviado',
+            message: 'O mapeamento foi enviado para o robô com sucesso!',
+          })()
+        "
+        color="primary"
+        label="Enviar mapeamento"
+        :disable="loading !== null || mappingRecords.length === 0"
+      />
+      <q-btn
+        @click="fetchMapping(false)"
+        color="primary"
+        label="Ler mapeamento"
+      />
+      <q-btn
+        @click="fetchMapping(true)"
+        color="primary"
+        label="Ler mapeamento na Ram"
+      />
+      <q-btn
+        @click="
+          performAction(
+            withSuccessFeedback(saveMapping, {
+              summary: 'Mapeamento Salvo',
+              message: 'O mapeamento enviado foi registrado com sucesso!',
+            }),
+            [],
+            {
+              title: 'Salvar Mapeamento',
+              question: 'Tem certeza que deseja salvar o mapeamento atual?',
+            }
+          )
+        "
+        color="primary"
+        label="Salvar mapeamento"
+        :disable="loading !== null"
+      />
       <q-dialog v-model="showErrorDialog">
         <CommandErrorCard :error="error" />
       </q-dialog>
-      <SuccessDialog v-model="showSuccessDialog" v-if="showSuccessDialog" :title="successDialogState.summary"
-        :message="successDialogState.message" />
-      <ConfirmActionDialog :confirm="confirm" v-if="isRevealed" :cancel="cancel" v-model="isRevealed">
+      <SuccessDialog
+        v-model="showSuccessDialog"
+        v-if="showSuccessDialog"
+        :title="successDialogState.summary"
+        :message="successDialogState.message"
+      />
+      <ConfirmActionDialog
+        :confirm="confirm"
+        v-if="isRevealed"
+        :cancel="cancel"
+        v-model="isRevealed"
+      >
         <template #title>{{ confirmDialogState.title }}</template>
         <template #question>{{ confirmDialogState.question }}</template>
       </ConfirmActionDialog>
 
-      <ProfileVersionsDialog collection="mappings" title="Mapeamentos" :data="mappingRecords" @install-request="(version: Robot.ProfileVersion<Robot.Mapping>) =>
+      <ProfileVersionsDialog
+        collection="mappings"
+        title="Mapeamentos"
+        :data="mappingRecords"
+        @install-request="(version: Robot.ProfileVersion<Robot.Mapping>) =>
         performAction(
           withSuccessFeedback(
             sendMapping,
@@ -82,10 +150,19 @@
             question: 'Tem certeza que deseja instalar a versão selecionada do mapeamento?'
           }
         )
-        " installable v-model="showVersionsDialog" v-slot="{ version }" v-if="showVersionsDialog">
+        "
+        installable
+        v-model="showVersionsDialog"
+        v-slot="{ version }"
+        v-if="showVersionsDialog"
+      >
         <q-list separator>
-          <q-expansion-item switch-toggle-side v-for="(record, index) of version.data" :key="index"
-            :caption="Number(index) || '0'">
+          <q-expansion-item
+            switch-toggle-side
+            v-for="(record, index) of version.data"
+            :key="index"
+            :caption="Number(index) || '0'"
+          >
             <q-list separator>
               <q-item v-for="(value, key) in record" :key="key">
                 <q-item-section>{{ key }}</q-item-section>
@@ -97,66 +174,113 @@
       </ProfileVersionsDialog>
     </div>
     <div class="q-pa-md q-gutter-sm">
-      <q-btn @click="
-        performAction(
-          async (id: number) => removeRecord(id),
-          [deleteRecordId],
-          {
-            title: 'Deletar Registro',
-            question: `Tem certeza que deseja deletar o registro ${deleteRecordId}`,
-          }
-        )
-        " color="primary" label="Deletar Registro" />
-      <q-btn @click="
-        performAction(
-          withSuccessFeedback(hardDeleteRecords, {
-            summary: 'Mapeamento Deletado',
-            message: 'O mapeamento salvo na flash foi deletado com sucesso.',
-          }),
-          [],
-          {
-            title: 'Deletar Mapeamento',
-            question: 'Tem certeza que deseja deletar o mapeamento atual?',
-          }
-        )
-        " color="primary" label="Deletar todos os registros" />
-      <q-select v-model="deleteRecordId" :options="recordDeleteOptions" :disable="recordDeleteOptions.length == 0"
-        label="Selecione o ID do registro que será deletado" />
+      <q-btn
+        @click="
+          performAction(
+            async (id: number) => removeRecord(id),
+            [deleteRecordId],
+            {
+              title: 'Deletar Registro',
+              question: `Tem certeza que deseja deletar o registro ${deleteRecordId}`,
+            }
+          )
+        "
+        color="primary"
+        label="Deletar Registro"
+      />
+      <q-btn
+        @click="
+          performAction(
+            withSuccessFeedback(hardDeleteRecords, {
+              summary: 'Mapeamento Deletado',
+              message: 'O mapeamento salvo na flash foi deletado com sucesso.',
+            }),
+            [],
+            {
+              title: 'Deletar Mapeamento',
+              question: 'Tem certeza que deseja deletar o mapeamento atual?',
+            }
+          )
+        "
+        color="primary"
+        label="Deletar todos os registros"
+      />
+      <q-select
+        v-model="deleteRecordId"
+        :options="recordDeleteOptions"
+        :disable="recordDeleteOptions.length == 0"
+        label="Selecione o ID do registro que será deletado"
+      />
     </div>
-    <q-table title="Adicionar Registro" :rows="newRecord" :columns="newColumns" row-key="id" binary-state-sort>
+    <q-table
+      title="Adicionar Registro"
+      :rows="newRecord"
+      :columns="newColumns"
+      row-key="id"
+      binary-state-sort
+    >
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="EncMedia" :props="props">
             {{ props.row.encMedia }}
-            <q-popup-edit v-model="props.row.encMedia" title="Atualizar Média dos encoders" buttons v-slot="scope">
+            <q-popup-edit
+              v-model="props.row.encMedia"
+              title="Atualizar Média dos encoders"
+              buttons
+              v-slot="scope"
+            >
               <q-input type="number" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
           <q-td key="Time" :props="props">
             {{ props.row.time }}
-            <q-popup-edit v-model="props.row.time" title="Atualizar o tempo" buttons v-slot="scope">
+            <q-popup-edit
+              v-model="props.row.time"
+              title="Atualizar o tempo"
+              buttons
+              v-slot="scope"
+            >
               <q-input type="number" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
 
           <q-td key="Offset" :props="props">
             {{ props.row.offset }}
-            <q-popup-edit v-model="props.row.offset" title="Atualizar Offset" buttons v-slot="scope">
+            <q-popup-edit
+              v-model="props.row.offset"
+              title="Atualizar Offset"
+              buttons
+              v-slot="scope"
+            >
               <q-input type="number" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
           <q-td key="TrackStatus" :props="props">
             {{ sectionOftheTrackToText(props.row.trackStatus) }}
-            <q-popup-edit v-model="props.row.trackStatus" title="Atualizar o Trackstatus" buttons v-slot="scope">
-              <q-select filled v-model="scope.value" :options="trackStatusOptions" emit-value map-options />
+            <q-popup-edit
+              v-model="props.row.trackStatus"
+              title="Atualizar o Trackstatus"
+              buttons
+              v-slot="scope"
+            >
+              <q-select
+                filled
+                v-model="scope.value"
+                :options="trackStatusOptions"
+                emit-value
+                map-options
+              />
             </q-popup-edit>
           </q-td>
         </q-tr>
-
       </template>
     </q-table>
     <div class="q-pa-md q-gutter-sm">
-      <q-btn @click="addMappingRecord" color="primary" label="Adicionar Registro" />
+      <q-btn
+        @click="addMappingRecord"
+        color="primary"
+        label="Adicionar Registro"
+      />
     </div>
   </div>
 </template>
@@ -270,10 +394,10 @@ const newRecord = ref<Robot.MappingRecord[]>([
   },
 ]);
 
-
 const sectionOftheTrackToText = (trackStatus: number) => {
-  const option = trackStatusOptions.find((option) => option.value == trackStatus);
+  const option = trackStatusOptions.find(
+    (option) => option.value == trackStatus
+  );
   return option ? option.label : 'Desconhecido';
-}
-
+};
 </script>
